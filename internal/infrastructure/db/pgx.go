b5656f773851c22b9936c530f78b54706776688e
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-04-28 01:07:32 +0200

    feat: add pgx

--- internal/infrastructure/db/pgx.go
+++ internal/infrastructure/db/pgx.go
@@ -1,1 +1,26 @@
 package db
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool(connString string) *pgxpool.Pool {
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		log.Fatal("unable to connect to database:", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		log.Fatal("database unreachable:", err)
+	}
+
+	log.Println("PostgreSQL connected (pgx)")
+
+	return pool
+}
