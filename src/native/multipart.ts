e24d778b19a5ed81ef27cc046528713eaced3e9f
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-06-24 03:46:08 +0200

    feat: rm native many complexity

--- src/native/multipart.ts
+++
@@ -1,6 +0,0 @@
-import { createRequire } from "node:module";
-import { join } from "node:path";
-import type { IncomingMessage } from "node:http";
-
-const require = createRequire(import.meta.url);
-const native = require(join(process.cwd(), "build/Release/multipart.node"));
